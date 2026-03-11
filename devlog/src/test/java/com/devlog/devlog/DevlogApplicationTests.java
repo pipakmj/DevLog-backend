43aadcdeb0b89f51d445c900e49ac3d05c48c41e
mjKim <106668821+pipakmj@users.noreply.github.com>
2026-03-12 00:22:04 +0900

    feat: 백엔드 초기 설정

---
+++ devlog/src/test/java/com/devlog/devlog/DevlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlog.devlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
