a59aa5b6d46ef86e014ae0e8393e844d6cb73034
mjKim <106668821+pipakmj@users.noreply.github.com>
2026-03-12 22:45:39 +0900

    feat: jwt인증 구현

---
+++ devlog/src/main/java/com/devlog/devlog/TestController.java
@@ -0,0 +1,23 @@
+package com.devlog.devlog;
+
+import com.devlog.devlog.global.provider.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class TestController {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @GetMapping("/auth/login-test")
+    public String loginTest(@RequestParam String id) {
+        return jwtTokenProvider.createToken(id);
+    }
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "hello";
+    }
+}
---
+++ devlog/src/main/java/com/devlog/devlog/global/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.devlog.devlog.global.config;
+
+import com.devlog.devlog.global.filter.JwtAuthenticationFilter;
+import com.devlog.devlog.global.provider.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+        private final JwtTokenProvider jwtTokenProvider;
+
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+                http
+                                .csrf(csrf -> csrf.disable())
+                                .sessionManagement(session -> session
+                                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                                .authorizeHttpRequests(auth -> auth
+                                                .requestMatchers("/auth/**").permitAll()
+                                                .anyRequest().authenticated())
+                                .addFilterBefore(
+                                                new JwtAuthenticationFilter(jwtTokenProvider),
+                                                UsernamePasswordAuthenticationFilter.class);
+
+                return http.build();
+        }
+}
